b2fd06171a2dd6bece898bd7117bffdf71675310
unknown <wangxuan@wangxuan-1.rdev.kingsoft.net>
2015-09-04 15:10:08 +0800

    SetUp

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager _instance;
+
+	public static GameManager Instance {
+		get { return _instance; }
+	}
+
+	void Awark() {
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/GamePanel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePanel : MonoBehaviour {
+
+	public static GamePanel _instance;
+
+	public static GamePanel Instance {
+		get { return _instance; }
+	}
+
+	void Awark() {
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
