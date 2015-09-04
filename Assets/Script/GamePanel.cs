d42528c55f78ca6be8302c358d41b1dfd77f9280
wangxuan <x.waston@gmail.com>
2015-09-04 16:31:39 +0800

    根据固定GameObject调整动态生成的GameObject位置初步试验成功

--- Assets/Script/GamePanel.cs
+++ Assets/Script/GamePanel.cs
@@ -9,13 +9,19 @@ public class GamePanel : MonoBehaviour {
 		get { return _instance; }
 	}
 
+	public GameObject prefabNumber;
+
 	void Awake() {
 		_instance = this;
 	}
 
 	// Use this for initialization
 	void Start () {
-	
+		var numcard = Instantiate(prefabNumber).GetComponent<RectTransform>();
+		var script = numcard.GetComponent<NumberCard>();
+		script.Init(2);
+		numcard.SetParent(gameObject.transform);
+		numcard.anchoredPosition = GameObject.Find("Bg_Obj1").GetComponent<RectTransform>().anchoredPosition;
 	}
 	
 	// Update is called once per frame
