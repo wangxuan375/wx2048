de12393344cfac639b1829e5be73d8815a55d67a
wangxuan <x.waston@gmail.com>
2015-09-09 20:09:35 +0800

    上下左右消息机制

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	static private GameManager _instance;
+
+	public static GameManager Instance {
+		get { return _instance; }
+	}
+
+	void Awake() {
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
